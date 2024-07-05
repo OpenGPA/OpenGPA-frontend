fd4d3288655dd5a219d26e99adb2aa1967125c48
OpenGPA <opengpa@163.com>
2024-07-05 17:52:02 +0800

    test: test for imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, Container, Typography } from '@mui/material';
+import { Line } from '@ant-design/charts';
 
 function App() {
+  const data = [
+    { year: '1991', value: 3 },
+    { year: '1992', value: 4 },
+    { year: '1993', value: 3.5 },
+    { year: '1994', value: 5 },
+    { year: '1995', value: 4.9 },
+    { year: '1996', value: 6 },
+    { year: '1997', value: 7 },
+    { year: '1998', value: 9 },
+    { year: '1999', value: 13 },
+  ];
+  const config = {
+    data,
+    title: {
+      visible: true,
+      text: 'Line Graph',
+    },
+    xField: 'year',
+    yField: 'value',
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="md" style={{ marginTop: 60 }}>
+      <Typography variant="h2" align="center" gutterBottom> OpenGPA </Typography>
+      <Button variant="contained" color="primary" fullWidth>Hello World</Button>
+      <Line {...config} />
+    </Container>
   );
 }
 
